350cb7592cba15afb1e2c26618bf2c23b9ce6d9a
Momtchil Momtchev <momtchil@momtchev.com>
2022-05-18 11:11:32 +0200

    fix page name

--- examples/App.tsx
+++ examples/App.tsx
@@ -37,7 +37,7 @@ const App = (): JSX.Element => {
     return (
         <Router>
             <h1 className='m-2'>
-                <strong>react-edit-list {pkgConfig.version} Examples</strong>
+                <strong>react-native-settings-screen {pkgConfig.version} Examples</strong>
             </h1>
             <div className='d-flex flex-row p-3'>
                 <div className='d-flex flex-column left-menu me-2'>
